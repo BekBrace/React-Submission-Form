e9644ad4ec9ce2ceaf3e86f5525b7804b0ee767a
BekBrace <bekdevelopment@gmail.com>
2022-06-26 05:40:56 -0500

    change 3: revert to useState

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,80 @@ import { useRef, useState } from 'react';
 
 
 function App() {
-  // const [username, setUsername] = useState(""); useState is used to re-render
+  // useState is used to re-render
+  // const [username, setUsername] = useState(""); 
   // useRef is used, if you don't want to re-render stuff
-  const usernameRef = useRef();
+  // const usernameRef = useRef();
+
+  const [values, setValues] = useState({
+    username:"",
+    email:"",
+    password:"",
+    confirmPassword:"",
+  });
+  
+const entries = [
+  {
+    id:1,
+    name:"username",
+    type:"text",
+    placeholder:"enter your username",
+    label:"Username"
+  },
+  {
+    id:2,
+    name:"email",
+    type:"text",
+    placeholder:"enter your email",
+    label:"E-Mail"
+  },
+  {
+    id:3,
+    name:"password",
+    type:"text",
+    placeholder:"enter your password",
+    label:"Password"
+  },
+  {
+    id:4,
+    name:"confirmPassword",
+    type:"text",
+    placeholder:"Confirm your pswd",
+    label:"Confirm"
+  }
+]
 
   // handleSubmit method to refresh the page  
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(usernameRef)
-    
+    // console.log(usernameRef)
+    // const data = new FormData(e.target);
+    // console.log(Object.fromEntries(data.entries()))
   }
 
+  // updating values entered by user in the form
+  //create onChange method for handling changes
+  //and set your values using setValues created above
+  //and then unpack the previous values, and for each target name
+  //you update them by vale :e.target.value
+  const onChange = (e) => {
+    setValues({...values, [e.target.name]:e.target.value});
+  }
+  console.log(values)
   return (
     <div className="app">
       <form onSubmit={handleSubmit}>
         {/* <INPUT placeholder="username" setUsername ={setUsername}/> */}
-        <INPUT refer={usernameRef} placeholder="username" />
-        <INPUT placeholder="email" />
-        <INPUT placeholder="password" />
-        <button>*submit*</button>
+        {/* <INPUT refer={usernameRef} placeholder="username" /> */}
+        {/* <INPUT name= "username" placeholder="username" />
+        <INPUT name= "email" placeholder="email" />
+        <INPUT name= "password" placeholder="password" /> */}
+        {entries.map((entry) => (
+          <INPUT key={entry.id} {...entry} 
+            value={values[entry.name]} 
+            onChange={onChange}/>
+        ))}
+        <button>SUBMIT</button>
       </form>
     </div>
   );
--- src/components/INPUT.jsx
+++ src/components/INPUT.jsx
@@ -3,13 +3,19 @@ import './INPUT.css'
 
 
 const INPUT = (props) => {
+  const {label, onChange, id, ...entryProps} = props;
   return (
     <div className='input'>
         {/* <label>Username</label> */}
         {/* <input placeholder={props.placeholder} onChange={e => props.setUsername(e.target.value)} /> */}
-        <input 
+        {/* <input 
         ref={props.refer}
-        placeholder={props.placeholder}/>
+        placeholder={props.placeholder}/> */}
+         {/* <input 
+        name={props.name}
+        placeholder={props.placeholder}/> */}
+      <label>{label}</label>
+      <input {...entryProps} onChange={onChange}/>
     </div>
   )
 }
